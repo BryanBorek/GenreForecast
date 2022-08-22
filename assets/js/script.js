bd412027c9df63577c3295a286b529efb82e7e0c
Bryan Borek <bryantborek@gmail.com>
2022-08-21 19:59:29 -0500

    added local storage featureto save playlists

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,15 @@ var resultsListEl = $('#results-list');
 var resultsItemEl = $('#results-item');
 var cardContainerEl = $('#cards')
 cardContainerEl.hide();
+var playlistContainerEl = $('#playlistContainer');
+playListBtn = $('#showPlaylistBtn');
+hidePlayListBtn =$('#hidePlaylistBtn');
+hidePlayListBtn.hide();
+
+var savedList = {
+    playlistName: $(JSON.parse(localStorage.getItem('savedplaylistName'))),
+    playlistLink: $(JSON.parse(localStorage.getItem('savedplaylistLink')))
+}
 
 // var for searches needs to be object
 var thunderstorm = {
@@ -68,6 +77,40 @@ var tornado = {
     id: ['alternative', 'hiphop', 'rock', 'punk', '0JQ5DAqbMKFIpEuaCnimBj']
 };
 
+playListBtn.on('click' , function(event) {
+    playlistContainerEl.children().remove();
+    event.preventDefault();
+    event.stopPropagation();
+    for (var i = 0; i < savedList.playlistName.length; i++) {
+        var choiceEl = $('<button>');
+        choiceEl.text(savedList.playlistName[i]);
+        choiceEl.attr('onclick', "location.href=" savedList.playlistLink[i]);
+        choiceEl.addClass('m-2.5 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"');
+        playlistContainerEl.append(choiceEl);
+    }
+    playListBtn.hide();
+    hidePlayListBtn.show();
+});
+
+hidePlayListBtn.on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    playlistContainerEl.children().remove();
+    hidePlayListBtn.hide();
+    playListBtn.show();
+});
+
+function updateSavedPlaylist() {
+    playlistContainerEl.children().remove();
+
+    for (var i = 0; i < savedList.playlistName.length; i++) {
+        var choiceEl = $('<button>');
+        choiceEl.text(savedList.playlistName[i]);
+        choiceEl.attr('onclick', 'location.href=' + savedList.playlistLink[i]);
+        choiceEl.addClass('m-2.5 inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"');
+        playlistContainerEl.append(choiceEl);
+    }
+}
 
 var token = "";
 
@@ -84,8 +127,7 @@ searchButtonEl.on('click', function(event) {
     var genreId;
     var genreName;
 
-
-    const _getPlaylistByGenre = async (token, genre) => {
+    const _getPlaylistByGenre = async (token, genreId) => {
         // On the line below, change 'alternative' to the variable genre when done with testing
         const result = await fetch('https://api.spotify.com/v1/browse/categories/' + genreId + '/playlists?limit=5', {
             method: 'GET',
@@ -94,19 +136,17 @@ searchButtonEl.on('click', function(event) {
 
         const data = await result.json();
         console.log(data);
-        $('#card1').text(data.playlists.items[0].name)
-        $('#image1').attr('src', data.playlists.items[0].images[0].url)
-        $('#desc1').text(data.playlists.items[0].description)
+        $('#card1').text(data.playlists.items[0].name);
+        $('#image1').attr('src', data.playlists.items[0].images[0].url);
+        $('#desc1').text(data.playlists.items[0].description);
         
-        $('#card2').text(data.playlists.items[1].name)
-        $('#image2').attr('src', data.playlists.items[1].images[0].url)
-        $('#desc2').text(data.playlists.items[1].description)
+        $('#card2').text(data.playlists.items[1].name);
+        $('#image2').attr('src', data.playlists.items[1].images[0].url);
+        $('#desc2').text(data.playlists.items[1].description);
         
-        $('#card3').text(data.playlists.items[2].name)
-        $('#image3').attr('src', data.playlists.items[2].images[0].url)
-        $('#desc3').text(data.playlists.items[2].description)
-
-        //$('#linkOne').attr('href' , data.playlists.items[0].external_urls.spotify)
+        $('#card3').text(data.playlists.items[2].name);
+        $('#image3').attr('src', data.playlists.items[2].images[0].url);
+        $('#desc3').text(data.playlists.items[2].description);
 
         cardContainerEl.show();
         $('#goto-playlist1').on('click', function() {
@@ -118,12 +158,75 @@ searchButtonEl.on('click', function(event) {
         $('#goto-playlist3').on('click', function() {
             window.open(data.playlists.items[2].external_urls.spotify, "_blank");
         });
+
+        $('#save-playlist1').on('click', function() {
+            event.preventDefault();
+            event.stopPropagation();
+            for (var i = 0; i < savedList.playlistLink.length; i++) {
+                for (var j = 0; j < savedList.playlistLink.length; j++) {
+                    if (data.playlists.items[0].external_urls.spotify === savedList.playlistLink[i]) {
+                        console.log('double')
+                        savedList.playlistLink.splice(i , 1);
+                        savedList.playlistName.splice(i , 1);
+                    }
+                }
+            }
+            savedList.playlistName.push(data.playlists.items[0].name);
+            savedList.playlistLink.push(data.playlists.items[0].external_urls.spotify);
+            localStorage.setItem('savedplaylistName', JSON.stringify(savedList.playlistName));
+            localStorage.setItem('savedplaylistLink', JSON.stringify(savedList.playlistLink));
+            updateSavedPlaylist()
+        });
+        $('#save-playlist2').on('click', function() {
+            event.preventDefault();
+            event.stopPropagation();
+            for (var i = 0; i < savedList.playlistLink.length; i++) {
+                for (var j = 0; j < savedList.playlistLink.length; j++) {
+                    if (data.playlists.items[1].external_urls.spotify === savedList.playlistLink[i]) {
+                        console.log('double')
+                        savedList.playlistLink.splice(i , 1);
+                        savedList.playlistName.splice(i , 1);
+                    }
+                }
+            }
+            savedList.playlistName.push(data.playlists.items[1].name);
+            savedList.playlistLink.push(data.playlists.items[1].external_urls.spotify);
+            localStorage.setItem('savedplaylistName', JSON.stringify(savedList.playlistName));
+            localStorage.setItem('savedplaylistLink', JSON.stringify(savedList.playlistLink));
+            updateSavedPlaylist()
+        });
+        $('#save-playlist3').on('click', function() {
+            event.preventDefault();
+            event.stopPropagation();
+            for (var i = 0; i < savedList.playlistLink.length; i++) {
+                for (var j = 0; j < savedList.playlistLink.length; j++) {
+                    if (data.playlists.items[2].external_urls.spotify === savedList.playlistLink[i]) {
+                        console.log('double')
+                        savedList.playlistLink.splice(i , 1);
+                        savedList.playlistName.splice(i , 1);
+                    }
+                }
+            }
+            savedList.playlistName.push(data.playlists.items[2].name);
+            savedList.playlistLink.push(data.playlists.items[2].external_urls.spotify);
+            localStorage.setItem('savedplaylistName', JSON.stringify(savedList.playlistName));
+            localStorage.setItem('savedplaylistLink', JSON.stringify(savedList.playlistLink));
+            updateSavedPlaylist()
+        });
+
         return data.playlists.items;
     }
 
     fetch(weatherSearchURL) 
     .then(function(res) {
-        return res.json()
+        if(res.status != '200') {
+            $('#weather-search').attr('placeholder', 'City not found, try again?');
+            console.log(res.cod);
+        }else{
+            $('#weather-search').attr('placeholder', 'Search again?');
+            $('#weather-search').val('');
+            return res.json();
+        }
     })
     .then(function(data) {
         console.log(data)
@@ -179,7 +282,7 @@ searchButtonEl.on('click', function(event) {
         genreId = currentWeather.id[i];
         genreName = currentWeather.name[i];
 
-        $('#forecastDesc').text(data.weather[0].main + ' in ' + city + ' today. In the mood for a ' + genreName + ' playlists.');
+        $('#forecastDesc').text(data.weather[0].main + ' in ' + city + ' today. In the mood for a ' + genreName + ' playlist?');
     })
     .then(function() {
         console.log('search initiated')
@@ -218,4 +321,3 @@ const APIController = (function() {
     }
     _getToken()
 })();
-
